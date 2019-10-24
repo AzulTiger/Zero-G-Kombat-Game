43859dc90cb08e4e882f3d89576709f884096c24
ines said <inessaid@mail.usf.edu>
2019-10-24 19:21:53 -0400

    generating ships spawnerr
    generates ships and send them to the playerr

--- SpaceTest1/Assets/spawner.cs
+++ SpaceTest1/Assets/spawner.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
+    public GameObject[] planes;
+    public Transform[] points;
+    public float beat = (60/130)*2;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer > beat)
+        {
+            GameObject plane = Instantiate(planes[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+            plane.transform.localPosition = Vector3.zero;
+            plane.transform.Rotate(transform.forward,90 * Random.Range(0,4));
+            timer -= beat;
+
+        }
+        timer += Time.deltaTime;
     }
 }
