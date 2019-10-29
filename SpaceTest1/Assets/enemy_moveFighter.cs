ab8d632d0a67061956548d767b103861a5af6808
AzulTiger <curtiss1@mail.usf.edu>
2019-10-28 23:20:51 -0400

    All Eyez on Me --Tupac
    Ships come after player

---
+++ SpaceTest1/Assets/enemy_moveFighter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_moveFighter : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float movementSpeed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+       // transform.rotation = new Quaternion(rotx, roty, rotz, rotw);
+        //transform.Rotate(transform.forward,90 * Random.Range(0,4));
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+
+
+    }
+}
