c0f8091a922c18a3e74e83677f17e7b2f62b6596
rjandplay <rprieto1000@gmail.com>
2019-11-11 14:40:27 -0500

    enemy shoot
    the enemy shoots at the player

---
+++ SpaceTest1/Assets/shooting/Enemy_Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+
+    private bool _alive;
+    public GameObject paintballPrefab;
+    private GameObject _paintball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<Playerinfo>())
+            {
+                if (_paintball == null)
+                {
+                    _paintball = Instantiate(paintballPrefab) as GameObject;
+                    _paintball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _paintball.transform.rotation = transform.rotation;
+                }
+            }
+        
+
+        }
+    }
+}
