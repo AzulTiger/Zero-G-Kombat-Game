1c49596a3492727770f9847cd645fdbd3532819f
ines said <inessaid@mail.usf.edu>
2019-11-12 21:28:00 -0500

    Revert "enemy movement"
    This reverts commit 202be9ad99f93ec2b494cd7f5fca8bc25889dc0a.

--- SpaceTest1/Assets/spawner.cs
+++ SpaceTest1/Assets/spawner.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public GameObject[] planes;
-    //public Transform[] points;
-    public GameObject player;
-    public Camera view;
-    public float spawnDistance;
+    public Transform[] points;
     public float beat = (60/130)*2;
     private float timer;
     // Start is called before the first frame update
@@ -20,16 +17,10 @@ public class spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Quaternion playerRotation = player.transform.rotation;
-        Vector3 point = new Vector3();
-        point = view.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), spawnDistance));
-
-
-
         if (timer > beat)
         {
-             Instantiate(planes[Random.Range(0, 2)], point, playerRotation );
-            //plane.transform.localPosition = Vector3.zero;
+            GameObject plane = Instantiate(planes[Random.Range(0, 2)], points[Random.Range(0, 6)]);
+            plane.transform.localPosition = Vector3.zero;
             //plane.transform.Rotate(transform.forward,90 * Random.Range(0,4));
             timer -= beat;
 
