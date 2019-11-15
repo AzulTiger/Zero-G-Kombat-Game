f44ea7f0d795792a907f55937d125b5331d54ed6
salmaeid <salmaeid@mail.usf.edu>
2019-11-14 20:24:19 -0500

    x and y spawner

--- SpaceTest1/Assets/spawner.cs
+++ SpaceTest1/Assets/spawner.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public GameObject[] planes;
-    public Transform[] points;
-    public float beat = (60/130)*2;
+    //public Transform[] points;
+    public GameObject player;
+    //public Camera view;
+    public float spawnDistance = 10f;
+    public float beat = (60 / 130) * 2;
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Quaternion playerRotation = player.transform.rotation;
+        //Vector3 point = new Vector3();
+        //point = view.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), spawnDistance));
+
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerDirection = player.transform.forward;
+        Quaternion playerRotation = player.transform.rotation;
+        //float spawnDistance = 10;
+
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+        //Vector3 spawnPos = playerPos;
+        spawnPos.x += Random.Range(-15f, 15f);
+        spawnPos.y += Random.Range(-10f, 10f);
+
+
+
+
+
+
+
+
         if (timer > beat)
         {
-            GameObject plane = Instantiate(planes[Random.Range(0, 2)], points[Random.Range(0, 6)]);
-            plane.transform.localPosition = Vector3.zero;
+            Instantiate(planes[Random.Range(0, 2)], spawnPos, playerRotation);
+            //plane.transform.localPosition = Vector3.zero;
             //plane.transform.Rotate(transform.forward,90 * Random.Range(0,4));
             timer -= beat;
 
